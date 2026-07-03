805729275b488e34a8efd5175b0a99716d9e17ac
alexandrejo-prog <alexandrejo@gmail.com>
2026-07-03 08:15:23 -0300

    feat: ativar botoes desfazer e refazer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./editor-scroll-fix";
+import "./editor-undo-redo";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
