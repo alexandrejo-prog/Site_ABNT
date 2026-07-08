43a7afa0887d5ad0396b4b552d79b7e93557389e
alexandrejo-prog <alexandrejo@gmail.com>
2026-07-08 12:22:49 -0300

    feat: carregar ajustes de acessibilidade

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ErrorBoundary } from "./ErrorBoundary";
 import "./styles.css";
+import "./accessibility.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
