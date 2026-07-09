ecdff8eb5d14d6ef8884da6df0bc7213ab8ced01
alexandrejo-prog <alexandrejo@gmail.com>
2026-07-09 19:16:59 -0300

    fix: evitar replaceAll no pos-processamento docx

--- src/export-graduate-editable-draft-docx.ts
+++ src/export-graduate-editable-draft-docx.ts
@@ -11,11 +11,16 @@ const APPROVAL_PLACEHOLDER_BEFORE = "Banca examinadora a ser preenchida na vers�
 const APPROVAL_PLACEHOLDER_AFTER =
   "Folha de aprovação provisória. Substitua os dados da banca após a aprovação ou conforme orientação do programa.";
 
+function replaceEvery(value: string, search: string, replacement: string): string {
+  return value.split(search).join(replacement);
+}
+
 function patchGraduateDocumentXml(xml: string): string {
-  return xml
-    .replaceAll(CATALOG_PLACEHOLDER_BEFORE, CATALOG_PLACEHOLDER_AFTER)
-    .replaceAll(APPROVAL_PLACEHOLDER_BEFORE, APPROVAL_PLACEHOLDER_AFTER)
-    .replaceAll('<w:ind w:firstLine="0" w:left="0"/>', '<w:ind w:left="0" w:hanging="720"/>');
+  return [
+    [CATALOG_PLACEHOLDER_BEFORE, CATALOG_PLACEHOLDER_AFTER],
+    [APPROVAL_PLACEHOLDER_BEFORE, APPROVAL_PLACEHOLDER_AFTER],
+    ['<w:ind w:firstLine="0" w:left="0"/>', '<w:ind w:left="0" w:hanging="720"/>'],
+  ].reduce((current, [search, replacement]) => replaceEvery(current, search, replacement), xml);
 }
 
 export async function generateGraduateEditableDraftDocxBlob(input: DocxGenerationInput): Promise<Blob> {
